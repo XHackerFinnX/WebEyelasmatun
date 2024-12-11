e81b2b3acfddf78006afe1887fc4901a4958ee53
xhackerfinnx <hfalex120@mail.ru>
2024-12-11 12:47:25 +0300

    Добавил конфиг и переменные

---
+++ app/config/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class Settings(BaseSettings):
+    POSTGRESQL_USER: SecretStr
+    POSTGRESQL_PASSWORD: SecretStr
+    POSTGRESQL_HOST: SecretStr
+    POSTGRESQL_PORT: SecretStr
+    POSTGRESQL_DATABASE: SecretStr
+    SECRET_AUTH: SecretStr
+    BOT_TOKEN: SecretStr
+    
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+    
+config = Settings()

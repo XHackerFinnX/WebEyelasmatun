7da9d6a55c418c651b6f381731fac09497b762db
xhackerfinnx <hfalex120@mail.ru>
2024-12-19 13:32:48 +0300

    добавление выпадающего меню и убрал кнопку выйти

--- app/static/js/user/user.js
+++ app/static/js/user/user.js
@@ -189,3 +189,27 @@ modalCloseButton.addEventListener('click', function () {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menuIcon');
+    const dropdownMenu = document.getElementById('dropdownMenu');
+
+    menuIcon.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownMenu.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+
+    // Close the menu when clicking outside
+    document.addEventListener('click', function(event) {
+        if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove('active');
+            menuIcon.classList.remove('active');
+        }
+    });
+
+    // Prevent clicks inside the dropdown menu from closing it
+    dropdownMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+});
+

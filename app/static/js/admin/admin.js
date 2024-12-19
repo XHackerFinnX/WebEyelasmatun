cb0f9bcdaecc5e708027f5aa855848e9cca46034
xhackerfinnx <hfalex120@mail.ru>
2024-12-19 13:33:19 +0300

    Выпдающее меню

--- app/static/js/admin/admin.js
+++ app/static/js/admin/admin.js
@@ -119,3 +119,57 @@ function handleResponse(tileId, data) {
             console.error('Unknown tile response');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menuIcon');
+    const dropdownMenu = document.getElementById('dropdownMenu');
+
+    menuIcon.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownMenu.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+
+    // Close the menu when clicking outside
+    document.addEventListener('click', function(event) {
+        if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove('active');
+            menuIcon.classList.remove('active');
+        }
+    });
+
+    // Prevent clicks inside the dropdown menu from closing it
+    dropdownMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+
+    function adjustDropdownPosition() {
+        const header = document.querySelector('header');
+        const dropdownMenu = document.getElementById('dropdownMenu');
+        dropdownMenu.style.top = header.offsetHeight + 'px';
+    }
+
+    // Call this function on load and resize
+    window.addEventListener('load', adjustDropdownPosition);
+    window.addEventListener('resize', adjustDropdownPosition);
+
+    const logoutButton = document.getElementById('logoutButton');
+    logoutButton.addEventListener('click', async function() {
+        console.log('User logged out');
+        try {
+            const response = await fetch('/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(),
+            });
+
+            if (response.ok) {
+                window.location.href = '/auth';
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+});

6983dabc653eae3aa057c2a936da35fec2656e62
xhackerfinnx <hfalex120@mail.ru>
2024-12-22 02:21:34 +0300

    init database

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+import psycopg2
+from config.config import config
+
+
+class Authentication:
+    def __init__(self):
+        self._connection = None
+        
+    def _connect(self):
+        if not self._connection or self._connection.closed:
+            self._connection = psycopg2.connect(
+                host=config.POSTGRESQL_HOST.get_secret_value(),
+                database=config.POSTGRESQL_DATABASE.get_secret_value(),
+                user=config.POSTGRESQL_USER.get_secret_value(),
+                password=config.POSTGRESQL_PASSWORD.get_secret_value(),
+                port=config.POSTGRESQL_PORT.get_secret_value()
+            )
+        return self._connection

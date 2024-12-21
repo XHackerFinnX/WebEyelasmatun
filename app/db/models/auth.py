797c19a69c05dafd52d24b0bbd6ed44d91570c7e
xhackerfinnx <hfalex120@mail.ru>
2024-12-22 02:22:23 +0300

    функция для проверки пользователя и для обновления статуса

---
+++ app/db/models/auth.py
@@ -0,0 +1,51 @@
+import psycopg2
+from psycopg2 import Error, InterfaceError
+from config.config import config
+from collections import namedtuple
+from datetime import datetime
+from db.database import Authentication
+
+Auth = Authentication()
+
+async def user_check_authentication(username: int, password: str):
+    
+    query = """
+    SELECT login
+    FROM eye_authentication 
+    WHERE login = %s
+    AND password = %s
+    """
+    
+    try:
+        with Auth._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (username, password))
+                login = cursor.fetchone()
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка проверки пользователя аутентификация {error}")
+    finally:
+        if Auth._connection:
+            Auth._connection.close()
+                
+        return login
+        
+        
+async def update_authentication(username: int, status: bool, last_date):
+    
+    query = """
+    UPDATE eye_authentication
+    SET (status, last_date) = (%s, %s)
+    WHERE login = %s
+    """
+    
+    try:
+        with Auth._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (status, last_date, username))
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка проверки пользователя аутентификация {error}")
+    finally:
+        if Auth._connection:
+            Auth._connection.close()

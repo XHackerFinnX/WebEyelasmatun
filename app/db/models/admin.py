cb8d37ba5c1939cca0cf5715b661be106d8d7f4d
xhackerfinnx <hfalex120@mail.ru>
2024-12-23 03:15:13 +0300

    управление добавлением, проверкой, обновлением и удалением даты для окошка

---
+++ app/db/models/admin.py
@@ -0,0 +1,97 @@
+import psycopg2
+from psycopg2 import Error, InterfaceError
+from config.config import config
+from collections import namedtuple
+from datetime import datetime
+from db.database import Admin
+
+
+Admin = Admin()
+
+async def admin_add_windows_day(date, time):
+    
+    query = """
+    INSERT INTO eye_windows (
+        date,
+        time
+    ) VALUES (%s, %s)
+    """
+    
+    try:
+        with Admin._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (date, time))
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка добавления окошек {error}")
+    finally:
+        if Admin._connection:
+            Admin._connection.close()
+                
+        return
+    
+
+async def check_windows_day(date):
+    
+    query = """
+    SELECT date
+    FROM eye_windows
+    WHERE date = %s
+    """
+    
+    try:
+        with Admin._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (date,))
+                day = cursor.fetchone()
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка проверки даты {error}")
+    finally:
+        if Admin._connection:
+            Admin._connection.close()
+                
+        return day
+    
+    
+async def update_windows_day(date, time):
+    
+    query = """
+    UPDATE eye_windows
+    SET (date, time) = (%s, %s)
+    WHERE date = %s
+    """
+    
+    try:
+        with Admin._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (date, time, date))
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка обновления окошек {error}")
+    finally:
+        if Admin._connection:
+            Admin._connection.close()
+                
+        return
+    
+
+async def admin_delete_windows_day(date):
+    
+    query = """
+    DELETE FROM eye_windows
+    WHERE date = %s
+    """
+    
+    try:
+        with Admin._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (date,))
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка удаления окошка {error}")
+    finally:
+        if Admin._connection:
+            Admin._connection.close()
+                
+        return

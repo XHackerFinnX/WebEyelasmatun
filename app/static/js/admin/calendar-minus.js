1c10598873a29daa4a77b7076f8c6c3e740f1f7e
xhackerfinnx <hfalex120@mail.ru>
2024-12-23 13:44:18 +0300

    всплывающее окно при удалении дня

--- app/static/js/admin/calendar-minus.js
+++ app/static/js/admin/calendar-minus.js
@@ -79,6 +79,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
             if (response.ok){
                 console.log('День удален');
+                alert('День удален');
             }
 
         } else {

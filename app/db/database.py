c193fb4371dcf263c62447249f03956e979b94bb
xhackerfinnx <hfalex120@mail.ru>
2024-12-23 14:23:29 +0300

    новый класс User

--- app/db/database.py
+++ app/db/database.py
@@ -48,3 +48,19 @@ class Admin:
                 port=config.POSTGRESQL_PORT.get_secret_value()
             )
         return self._connection
+    
+
+class User:
+    def __init__(self):
+        self._connection = None
+        
+    def _connect(self):
+        if not self._connection or self._connection.closed:
+            self._connection = psycopg2.connect(
+                host=config.POSTGRESQL_HOST.get_secret_value(),
+                database=config.POSTGRESQL_DATABASE.get_secret_value(),
+                user=config.POSTGRESQL_USER.get_secret_value(),
+                password=config.POSTGRESQL_PASSWORD.get_secret_value(),
+                port=config.POSTGRESQL_PORT.get_secret_value()
+            )
+        return self._connection

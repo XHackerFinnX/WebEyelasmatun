c5a5b20d215e0eb59a9c5f76c7afa2c7fb056a18
xhackerfinnx <hfalex120@mail.ru>
2024-12-23 03:15:33 +0300

    Отображение даты и времени в главном меню

---
+++ app/db/models/main_windows.py
@@ -0,0 +1,54 @@
+import psycopg2
+from psycopg2 import Error, InterfaceError
+from config.config import config
+from collections import namedtuple
+from datetime import datetime
+from db.database import Windows
+
+
+Win = Windows()
+
+async def windows_day():
+    
+    query = """
+    SELECT date
+    FROM eye_windows
+    WHERE date >= %s
+    """
+    
+    try:
+        with Win._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (datetime.today(),))
+                day = cursor.fetchall()
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка получения дат для отображения свободных окошек {error}")
+    finally:
+        if Win._connection:
+            Win._connection.close()
+                
+        return day
+    
+
+async def windows_day_time(date):
+    
+    query = """
+    SELECT time
+    FROM eye_windows
+    WHERE date = %s
+    """
+    
+    try:
+        with Win._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (date,))
+                time = cursor.fetchone()
+            conn.commit()
+    except (InterfaceError, Error) as error:
+        print(f"Ошибка получения времени для окошек {error}")
+    finally:
+        if Win._connection:
+            Win._connection.close()
+                
+        return time[0]

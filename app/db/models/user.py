c2859b543cb808890c632045614316bb0ed4299d
xhackerfinnx <hfalex120@mail.ru>
2024-12-24 23:59:00 +0300

    передача ip адреса в БД UPDATE

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -5,27 +5,15 @@ from db.database import User
 
 User = User()
 
-# async def add_user(
-    
-# ):
-
 async def update_ip_user(data_ip, id_user: int):
-    
     query = """
     UPDATE profile_user
-    SET ip_address = %s
-    WHERE id = %s
+    SET ip_address = $1
+    WHERE id = $2
     """
-    
     try:
-        with User._connect() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(query, (data_ip, id_user))
-            conn.commit()
-    except (InterfaceError, Error) as error:
-        print(f"Ошибка обновления ip адреса {error}")
-    finally:
-        if User._connection:
-            User._connection.close()
-                
-        return
+        pool = await User.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(query, data_ip, id_user)
+    except Exception as error:
+        print(f"Ошибка обновления IP-адреса: {error}")

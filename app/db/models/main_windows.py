094ddcbd6b3ce7b68e8dcd663aadcd739afaba0d
xhackerfinnx <hfalex120@mail.ru>
2024-12-27 01:23:32 +0300

    Функции для работы с базой данных price_list

--- app/db/models/main_windows.py
+++ app/db/models/main_windows.py
@@ -54,3 +54,102 @@ async def update_time_in_day(date, time):
             await conn.execute(query, time, date)
     except Exception as error:
         print(f"Ошибка обновления времени для окошка: {error}")
+        
+        
+async def price_list_load():
+    
+    query = """
+    SELECT id_name, name, price
+    FROM price_list
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            pricel = await conn.fetch(query)
+            return pricel
+    except Exception as error:
+        print(f"Ошибка получения прайс листа: {error}")
+        return None
+    
+    
+async def price_list_add(id_name: int, name: str, price: int):
+    
+    query = """
+    INSERT INTO price_list (
+        id_name, name, price
+    ) VALUES ($1, $2, $3)
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(query, id_name, name, price)
+    except Exception as error:
+        print(f"Ошибка добавление прайс листа: {error}")
+        
+
+async def price_list_check(id_name: int):
+    
+    query = """
+    SELECT id_name, name, price
+    FROM price_list
+    WHERE id_name = $1
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            price_check = await conn.fetch(query, id_name)
+            return True if price_check else False
+    except Exception as error:
+        print(f"Ошибка проверки прайс листа: {error}")
+        return None
+    
+    
+async def price_list_update(id_name: int, name: str, price: int):
+    
+    query = """
+    UPDATE price_list
+    SET (name, price) = ($1, $2)
+    WHERE id_name = $3
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(query, name, price, id_name)
+    except Exception as error:
+        print(f"Ошибка обновления прайс листа: {error}")
+        
+        
+async def price_list_delete(id_name: int):
+    
+    query = """
+    DELETE FROM price_list
+    WHERE id_name = $1
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(query, id_name)
+    except Exception as error:
+        print(f"Ошибка удаления прайс листа: {error}")
+        
+        
+async def price_list_select_user():
+    
+    query = """
+    SELECT id_name, name, price
+    FROM price_list
+    """
+    
+    try:
+        pool = await Win.connect()
+        async with pool.acquire() as conn:
+            pricel = await conn.fetch(query)
+            return pricel
+    except Exception as error:
+        print(f"Ошибка получения прайс листа для клиентов: {error}")
+        return None

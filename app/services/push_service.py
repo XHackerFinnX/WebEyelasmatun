442c66f55ce88395c0e914900357b3dd51e0c9c5
xhackerfinnx <hfalex120@mail.ru>
2024-12-28 16:57:57 +0300

    добавил в логику уведомлений функции для изменений данных у профиля пользователя. А также просмотр статуса, чтобы предотвратить присылание уведомления

--- app/services/push_service.py
+++ app/services/push_service.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 from app.services.bot_notice import send_message_record, send_message_record_admin
 from app.db.models.user import (update_status_record_user, profile_record_user,
-                                last_record_user, update_profile_last_record_user)
+                                last_record_user, update_profile_last_record_user,
+                                check_record_user_before_notification, count_add_visits)
 import asyncio
 
 async def push_sms(id_user, date):
@@ -20,18 +21,46 @@ async def push_sms(id_user, date):
     if date_today.date() == date.date():
         counting_down = date - date_today - timedelta(hours=1)
         await asyncio.sleep(counting_down.seconds)
+        
+        # Проверка. Есть ли запись у клиента
+        status = await check_record_user_before_notification(id_user, date_r, time_r)
+        if not status:
+            return
+
+        # Отправка уведомления
         await send_message_record(id_user, text)
+        
+        #Обновление статуса - запись завершена
         await update_status_record_user(id_user, date_r, time_r)
+        
+        # Последняя записи дата
         last_r_user = await last_record_user(id_user)
         await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+        
+        # Добавляется +1 к посещению
+        await count_add_visits(id_user)
         
     elif date_today.date() == date.date() - timedelta(days=1):
         counting_down = date - date_today - timedelta(hours=1)
         await asyncio.sleep(counting_down.seconds)
+        
+        # Проверка. Есть ли запись у клиента
+        status = await check_record_user_before_notification(id_user, date_r, time_r)
+        if not status:
+            return
+
+        # Отправка уведомления
         await send_message_record(id_user, text)
+        
+        #Обновление статуса - запись завершена
         await update_status_record_user(id_user, date_r, time_r)
+        
+        # Последняя записи дата
         last_r_user = await last_record_user(id_user)
         await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+        
+        # Добавляется +1 к посещению
+        await count_add_visits(id_user)
         
     elif date_today.date() < date.date():
         counting_down = date - date_today - timedelta(days=1)
@@ -41,10 +70,24 @@ async def push_sms(id_user, date):
         date_today = datetime.now()
         counting_down = date - date_today - timedelta(hours=1)
         await asyncio.sleep(counting_down.seconds)
+        
+        # Проверка. Есть ли запись у клиента
+        status = await check_record_user_before_notification(id_user, date_r, time_r)
+        if not status:
+            return
+
+        # Отправка уведомления
         await send_message_record(id_user, text)
+        
+        #Обновление статуса - запись завершена
         await update_status_record_user(id_user, date_r, time_r)
+        
+        # Последняя записи дата
         last_r_user = await last_record_user(id_user)
         await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+        
+        # Добавляется +1 к посещению
+        await count_add_visits(id_user)
         
     return
 
@@ -59,18 +102,60 @@ async def push_sms_technic(id_user, date):
     if date_today.date() == date.date():
         counting_down = date - date_today - timedelta(hours=1)
         if counting_down.seconds < 0:
+            
+            # Отправка уведомления
             await send_message_record(id_user, text)
+            
+            #Обновление статуса - запись завершена
             await update_status_record_user(id_user, date_r, time_r)
+            
+            # Последняя записи дата
+            last_r_user = await last_record_user(id_user)
+            await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+            
+            # Добавляется +1 к посещению
+            await count_add_visits(id_user)
         else:
-            await asyncio.sleep(counting_down.seconds)
+            
+            # Проверка. Есть ли запись у клиента
+            status = await check_record_user_before_notification(id_user, date_r, time_r)
+            if not status:
+                return
+
+            # Отправка уведомления
             await send_message_record(id_user, text)
+            
+            #Обновление статуса - запись завершена
             await update_status_record_user(id_user, date_r, time_r)
+            
+            # Последняя записи дата
+            last_r_user = await last_record_user(id_user)
+            await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+            
+            # Добавляется +1 к посещению
+            await count_add_visits(id_user)
         
     elif date_today.date() == date.date() - timedelta(days=1):
         counting_down = date - date_today - timedelta(hours=1)
         await asyncio.sleep(counting_down.seconds)
+        
+        # Проверка. Есть ли запись у клиента
+        status = await check_record_user_before_notification(id_user, date_r, time_r)
+        if not status:
+            return
+
+        # Отправка уведомления
         await send_message_record(id_user, text)
+        
+        #Обновление статуса - запись завершена
         await update_status_record_user(id_user, date_r, time_r)
+        
+        # Последняя записи дата
+        last_r_user = await last_record_user(id_user)
+        await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+        
+        # Добавляется +1 к посещению
+        await count_add_visits(id_user)
         
     elif date_today.date() < date.date():
         counting_down = date - date_today - timedelta(days=1)
@@ -80,8 +165,24 @@ async def push_sms_technic(id_user, date):
         date_today = datetime.now()
         counting_down = date - date_today - timedelta(hours=1)
         await asyncio.sleep(counting_down.seconds)
+        
+        # Проверка. Есть ли запись у клиента
+        status = await check_record_user_before_notification(id_user, date_r, time_r)
+        if not status:
+            return
+
+        # Отправка уведомления
         await send_message_record(id_user, text)
+        
+        #Обновление статуса - запись завершена
         await update_status_record_user(id_user, date_r, time_r)
+        
+        # Последняя записи дата
+        last_r_user = await last_record_user(id_user)
+        await update_profile_last_record_user(id_user, last_r_user[0]['date'])
+        
+        # Добавляется +1 к посещению
+        await count_add_visits(id_user)
         
     return
         

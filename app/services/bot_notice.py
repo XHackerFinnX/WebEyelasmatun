338f723ebd1362d8cfd2e955f8c842a3477b8df6
xhackerfinnx <hfalex120@mail.ru>
2024-12-28 04:14:12 +0300

    файл для отправки сообщений через бота

---
+++ app/services/bot_notice.py
@@ -0,0 +1,78 @@
+from config.config import config
+
+import requests
+import asyncio
+
+API_URL = 'https://api.telegram.org/bot'
+
+async def send_message_mail(chat_id_list: list, chat_id_admin: list, text: str):
+    
+    mail_yes = 0
+    mail_no = 0
+    list_chat_id = []
+    
+    for chat_id in chat_id_list:
+        print(chat_id[0])
+        try:
+            message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={chat_id[0]}&text={text}'
+            requests.get(message)
+            mail_yes += 1
+        except:
+            mail_no += 1
+            list_chat_id.append(chat_id)
+        await asyncio.sleep(0.5)
+    
+    text = f"Рассылку получили:\nВсего пользователей: {mail_yes+mail_no}\nПолучили: {mail_yes}\nНе получили: {mail_no}"
+    
+    for admin_id in chat_id_admin:
+        
+        try:
+            message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={admin_id}&text={text}'
+            requests.get(message)
+        except:
+            print('Админа нет')
+        await asyncio.sleep(0.5)
+    
+    return
+
+
+async def send_message_private(chat_id: int, admin_id: int, text: str):
+    
+    try:
+        message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={chat_id}&text={text}'
+        requests.get(message)
+        text = "Сообщение клиент получил!"
+    except:
+        text = "Сообщение клиент не получил! Возможно клиент заблокировал бота."
+    
+
+    message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={admin_id}&text={text}'
+    requests.get(message)
+    
+    return
+
+
+async def send_message_delete_admin(chat_id: int, text: str):
+
+    try:
+        message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={chat_id}&text={text}'
+        requests.get(message)
+    except:
+        pass
+    
+    return
+
+
+async def send_message_delete_user(admin_id_list: list, text: str):
+    
+    for admin_id in admin_id_list:
+        
+        try:
+            message = f'{API_URL}{config.BOT_TOKEN.get_secret_value()}/sendMessage?chat_id={admin_id}&text={text}'
+            requests.get(message)
+        except:
+            print('Админа нет')
+        await asyncio.sleep(0.5)
+    
+    return
+    

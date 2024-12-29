2dc221b393466701378ba0d0bc90b8ba856b4ef0
xhackerfinnx <hfalex120@mail.ru>
2024-12-29 12:46:58 +0300

    функция post запрос на загрузку отзыва и функция post запрос для выгрзки отзывов на сайт

--- app/routers/feedback_rout.py
+++ app/routers/feedback_rout.py
@@ -1,30 +1,24 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from typing import List
 from app.routers.auth_rout import get_current_user
-from datetime import datetime, timedelta
-from app.db.models.admin import (admin_add_windows_day, check_windows_day,
-                             update_windows_day, admin_delete_windows_day,
-                             schedule_record_user, admin_list, select_user_all,
-                             black_list_user, select_user_all_blacklist_true,
-                             select_user_all_blacklist_false, select_user_all_delete,
-                             select_user, select_history_user, update_history_user_comment_admin,
-                             update_history_user_money, select_date_money)
-from app.db.models.user import select_record_user, delete_record_user, technical_record_user, count_del_visits
-from app.db.models.main_windows import update_time_in_day, windows_day_time
-from collections import namedtuple
-from app.services.bot_notice import send_message_mail, send_message_private, send_message_delete_admin
-from app.services.push_service import push_sms_technic
-
-import calendar, locale
-import asyncio
+from datetime import datetime
+from app.db.models.admin import admin_list
+from app.db.models.user import add_feedback_user, all_feedback_user, select_user_photo
 
 router = APIRouter(
     prefix="",
     tags=["Admin"]
 )
+
+class FeedbackUser(BaseModel):
+    text: str
+    rating: int
+    
+class FeedbackFilter(BaseModel):
+    filteruser: str
+    page: int
 
 templates_admin = Jinja2Templates(directory=r"./app/templates/admin")
 templates_main = Jinja2Templates(directory=r"./app/templates/main")
@@ -89,3 +83,55 @@ async def feedback_get(request: Request, id_user: int, user: dict = Depends(get_
     
     else:
         return templates_auth.TemplateResponse("auth.html", {"request": request})
+    
+    
+@router.post('/api/feedback/download')
+async def feedback_download(data: FeedbackUser, user: dict = Depends(get_current_user)):
+    
+    if user:
+        status = await add_feedback_user(user, data.text, data.rating, datetime.now().date())
+        if status['status'] == 'No feedback':
+            return JSONResponse(content={'status': False}, status_code=401)
+        
+        return JSONResponse(content={'status': True})
+        
+    else:
+        return JSONResponse(content={'status': False}, status_code=401)
+    
+    
+@router.post('/api/feedback/load')
+async def feedback_download(data: FeedbackFilter, user: dict = Depends(get_current_user)):
+    
+    if user:
+        
+        all_feedback = await all_feedback_user()
+        photo_user = await select_user_photo()
+        
+        list_feedback = []
+        for lf in all_feedback:
+            
+            for pu in photo_user:
+                if pu['id'] == lf['id']:
+                    avatar = avatar = f'http://127.0.0.1:8000/api/photos/{pu['id']}'
+                    break
+            else:
+                avatar = '/app/static/images/feedback.png'
+                
+            user_d = {
+                'id': lf['id'],
+                'name': lf['name'],
+                'date': lf['date_feedback'].strftime("%Y-%m-%d"),
+                'avatar': avatar,
+                'rating': lf['stars'],
+                'text': lf['text_feedback']
+            }
+            list_feedback.append(user_d)
+            
+        return JSONResponse(content={
+            'feedback': list_feedback,
+            'hasMore': True,
+            'userId': user
+        })
+        
+    else:
+        return JSONResponse(content=[], status_code=401)

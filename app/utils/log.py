0436d7ac95c13bfff9b687c2dd0e6994aaf25739
xhackerfinnx <hfalex120@mail.ru>
2025-01-08 01:42:03 +0300

    обновление даты логера на Europe/Moscow

--- app/utils/log.py
+++ app/utils/log.py
@@ -1,14 +1,25 @@
+from zoneinfo import ZoneInfo
 import logging
+from datetime import datetime
 
 def setup_logger(name):
-    
+    piter_tz = ZoneInfo("Europe/Moscow")
+
+    class PiterTimezoneFormatter(logging.Formatter):
+        def formatTime(self, record, datefmt=None):
+            # Преобразуем время записи лога в указанный часовой пояс
+            dt = datetime.fromtimestamp(record.created, tz=piter_tz)
+            if datefmt:
+                return dt.strftime(datefmt)
+            return dt.isoformat()
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter(
+    formatter = PiterTimezoneFormatter(
         '%(asctime)s - %(name)s %(levelname)s: %(message)s',
         datefmt='%d.%m.%Y %H:%M:%S'
     )
